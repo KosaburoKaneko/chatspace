1e802dc9e8fd24b40acf1fb6560ed47d4de38e25
Kosaburo Kaneko <b1840943@gmail.com>
2018-05-15 19:54:18 +0900

    Change href to pathname

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -23,9 +23,9 @@ $(document).on("turbolinks:load", function() {
   }
 
   var interval = setInterval(function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    if (window.location.pathname.match(/\/groups\/\d+\/messages/)) {
   $.ajax({
-    url: location.href,
+    url: location.pathname,
     dataType: "json"
   })
   .done(function(json) {
