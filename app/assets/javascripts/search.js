23d066d3df68835a7d68ee0136f7009985689d20
Kosaburo Kaneko <b1840943@gmail.com>
2018-05-15 16:38:37 +0900

    Change to .on because jQuery is used for other parts

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,4 @@
-document.addEventListener("turbolinks:load", function() {
+$(document).on("turbolinks:load", function() {
   $(function() {
     function removeResult() {
       $('#user-search-result').children().remove();
